d842ef809c74f7d4c60103ef9b7b642ca2204c7b
Vladyslav Smyrnov <vladyslav.smyrnov@nure.ua>
2018-10-21 10:50:29 +0300

    initial commit

---
+++ Passkeeper/Core/Passkeeper.cs
@@ -0,0 +1,25 @@
+﻿namespace Passkeeper.Core
+{
+	class Passkeeper
+	{
+		public void ExtractFromFile()
+		{
+
+		}
+
+		public void GetCurrentPassword( string _id )
+		{
+
+		}
+
+		public void GetAllPasswords( string _id )
+		{
+
+		}
+
+		public void Access()
+		{
+
+		}
+	}
+}
---
+++ Passkeeper/Core/PasswordChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passkeeper.Core
+{
+	class PasswordChecker
+	{
+		public void ReadPassword()
+		{
+
+		}
+
+		private void CheckPAssword()
+		{
+
+		}
+	}
+}
